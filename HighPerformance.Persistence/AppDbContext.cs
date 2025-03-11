e22c601d36c23f4430da44ead65cdeacfa890fda
Elio Cortés <elio.cortes3000@gmail.com>
2025-03-10 21:49:11 -0500

    Created migration

--- HighPerformance.Persistence/AppDbContext.cs
+++ HighPerformance.Persistence/AppDbContext.cs
@@ -5,16 +5,25 @@ namespace HighPerformance.Persistence
 {
     public class AppDbContext : DbContext
     {
-
         public AppDbContext()
         {
-            
         }
+
         public AppDbContext(DbContextOptions<AppDbContext> options)
            : base(options)
         {
         }
+
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=HighPerformance.db");
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
