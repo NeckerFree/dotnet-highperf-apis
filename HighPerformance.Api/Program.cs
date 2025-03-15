cf9549343d9a1b3102312feea652e82f11d6ea7e
Elio Cortés <elio.cortes3000@gmail.com>
2025-03-14 23:24:31 -0500

    Update .gitignore to include new patterns and files
    - 🚫 Ignore files with extensions `.cache` and `.bin`
    - 📁 Ignore Visual Studio related directories in `HighPerformanceApis`
    - 🗂️ Ignore Visual Studio solution user options file `.suo`
    - 🚫 Ignore migration files in `HighPerformance.Persistence/Migrations`
    - 🚫 Ignore specific migration file `20250315033656_InitialCreate.cs`
    
    - EC

--- HighPerformance.Api/Program.cs
+++ HighPerformance.Api/Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Register the DbContext with SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -20,7 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
