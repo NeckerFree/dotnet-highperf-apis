7866ea6bba6689eb3648218177de310274822893
Elio Cortés <elio.cortes3000@gmail.com>
2025-03-24 00:15:49 -0500

    Exception handling (#7)
    * added exception handling
    
    * Fixed warninigs

--- HighPerformance.Api/Program.cs
+++ HighPerformance.Api/Program.cs
@@ -1,10 +1,11 @@
+using HighPerformance.Api.Logger;
+using HighPerformance.Api.Middleware;
 using HighPerformance.Application;
 using HighPerformance.Application.Interfaces;
 using HighPerformance.Application.Mappings;
 using HighPerformance.Persistence;
 using HighPerformance.Persistence.Repositories;
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddSwaggerGen();
 
 // Register Application Layer services
@@ -15,6 +16,9 @@ builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddAutoMapper(typeof(ProductProfile).Assembly);
 builder.Services.AddControllers();
+// Register the custom file logger provider
+builder.Logging.ClearProviders(); // Clear default providers
+builder.Logging.AddProvider(new FileLoggerProvider($"Logs/log-{DateTime.Now:yyyy-MM-dd}.txt"));
 
 // Add CORS policy
 //builder.Services.AddCors(options =>
@@ -35,10 +39,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.MapControllers();
 
