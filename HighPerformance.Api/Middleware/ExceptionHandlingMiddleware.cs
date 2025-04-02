f864b951096639c1d43bb173aa9e63b589a5dd7a
Elio Cortés <elio.cortes3000@gmail.com>
2025-04-02 00:58:35 -0500

    Implemented ValidationException

--- HighPerformance.Api/Middleware/ExceptionHandlingMiddleware.cs
+++ HighPerformance.Api/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,19 +1,71 @@
-﻿namespace HighPerformance.Api.Middleware
+﻿using FluentValidation;
+using HighPerformance.Domain.Exceptions.Base;
+using System;
+using System.Net;
+using System.Text.Json;
+
+namespace HighPerformance.Api.Middleware
 {
     public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
     {
         public async Task InvokeAsync(HttpContext context)
         {
-			try
-			{
-				await next(context);
-			}
-			catch (Exception ex)
-			{
-				logger.LogError(ex, "An unhandled exception occurred.");
-				context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-				await context.Response.WriteAsync("An unexpected error occurred.");
-			}
+            try
+            {
+                await next(context);
+            }
+            catch (Exception exception)
+            {
+                logger.LogError(exception, "An exception occurred: {Message}", exception.Message);
+
+                var statusCode = GetStatusCode(exception);
+                var response = CreateResponse(exception, statusCode);
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = statusCode;
+
+                await context.Response.WriteAsync(response);
+               
+            }
+        }
+
+
+
+        private static int GetStatusCode(Exception exception) =>
+         exception switch
+         {
+             ValidationException => (int)HttpStatusCode.BadRequest, // 400
+             NotFoundException =>(int)HttpStatusCode.NotFound, //404
+             _ => (int)HttpStatusCode.InternalServerError // 500 (default)
+         };
+
+        private static string CreateResponse(Exception exception, int statusCode)
+        {
+            return exception switch
+            {
+                ValidationException validationException => JsonSerializer.Serialize(new
+                {
+                    StatusCode = statusCode,
+                    Message = "Validation failed",
+                    Errors = validationException.Errors.Select(e => new
+                    {
+                        e.PropertyName,
+                        e.ErrorMessage
+                    })
+                }),
+
+                NotFoundException notFoundException => JsonSerializer.Serialize(new
+                {
+                    StatusCode = statusCode,
+                    Message = "Not Found",
+                    
+                }),
+                _ => JsonSerializer.Serialize(new
+                {
+                    StatusCode = statusCode,
+                    Message = "An error occurred"
+                })
+            };
         }
     }
 }
